b31b75f4bd0fd8b351508dde951c28556f59c436
Gilles Bosmans <91054591+GillesBosmans@users.noreply.github.com>
2024-12-19 00:13:15 +0100

    Delete formule1-frontend directory

--- formule1-frontend/src/pages/Results.d.ts
+++
@@ -1,6 +0,0 @@
-import { User } from '../types';
-interface ResultsProps {
-    user: User | null;
-}
-export default function Results({ user }: ResultsProps): import("react/jsx-runtime").JSX.Element;
-export {};
--- formule1-frontend/src/pages/Tracks.d.ts
+++
@@ -1,6 +0,0 @@
-import { User } from '../types';
-interface TracksProps {
-    user: User | null;
-}
-export default function Tracks({ user }: TracksProps): import("react/jsx-runtime").JSX.Element;
-export {};
