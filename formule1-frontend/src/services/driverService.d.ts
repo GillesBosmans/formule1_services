9e195d73bcfa1b02a214a3e2195a0ea8995f4b85
Gilles Bosmans <91054591+GillesBosmans@users.noreply.github.com>
2024-12-19 00:12:19 +0100

    Delete formule1-frontend directory

--- formule1-frontend/src/services/driverService.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const getDrivers: () => Promise<any>;
-export declare const createDriver: (driver: any) => Promise<any>;
-export declare const updateDriver: (id: number, driver: any) => Promise<any>;
-export declare const deleteDriver: (id: number) => Promise<void>;
--- formule1-frontend/src/services/teamService.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const getTeams: () => Promise<any>;
-export declare const createTeam: (team: any) => Promise<any>;
-export declare const updateTeam: (id: number, team: any) => Promise<any>;
